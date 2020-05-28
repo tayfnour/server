2837d0f730cd659d86d1486f129ec86e851eaf87
tayfNour <abo.omar.tayf@gmail.com>
2020-05-28 21:28:07 +0300

    initial commit 7

---
+++ server.js
@@ -0,0 +1,48 @@
+var WebSocketServer = require('ws').Server
+, http = require('http')
+, express = require('express')
+, app = express()
+, port = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + '/'));
+
+var server = http.createServer(app);
+
+server.listen(port);
+
+var wss = new WebSocketServer({server: server});
+
+//var HOST = location.origin.replace(/^http/, 'ws');
+
+wss.on('connection', function (ws) {
+
+    console.log('client connected');
+
+    ws.on('message', function (message) {
+        console.log(message);
+
+        wss.broadcast(message);
+    });
+
+    ws.send("Server :I'm Ready...");
+    console.log(wss.clients);
+
+
+});
+
+wss.broadcast = function broadcast(msg) {
+    console.log(msg);
+    wss.clients.forEach(function each(client) {
+        client.send(msg);
+    });
+
+}
+
+wss.addEventListener('hassan', (data) => {
+    // data.data contains your forwarded data
+    console.log(data.data)
+})
+
+wss.on('hassan', function (ws) {
+    ws.send("hassan event");
+});
